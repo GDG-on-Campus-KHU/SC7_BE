5ae112dabe6c8f929c3aa5b106bcc7e542a8eae6
gleaming9 <gleaming9@khu.ac.kr>
2024-11-27 22:48:36 +0900

    fix: synchronization between Get and Delete

--- handler/post_handler.go
+++ handler/post_handler.go
@@ -6,7 +6,17 @@ import (
 	"github.com/gin-gonic/gin"
 	"net/http"
 	"path/filepath"
+	"sync"
 )
+
+// delete 와 get 사이의 동기화를 위한 뮤텍스
+var mu sync.Mutex
+
+/*
+ create 과 get 사이의 동기화는 AI 서버로 부터 accuracy 값을 받아오는 것을 기다리는 것으로 구현
+ 단순히 create 이 완료될 때까지 기다렸다가 get 을 하게 되면, AI 서버로 부터 accuracy 값을 받아오기 전에 get 이 되어버리기 때문에
+ mutex lock 을 사용하는 것이 아니라 AI 서버로부터 accuracy 값이 갱신될 때까지 대기하도록 구현
+*/
 
 // CreatePost 핸들러: 게시글 생성
 func CreatePost(c *gin.Context) {
@@ -45,6 +55,10 @@ func CreatePost(c *gin.Context) {
 // GetPost 핸들러: 게시글 조회
 func GetPost(c *gin.Context) {
 	id := c.Param("id")
+
+	mu.Lock()         // get 이 끝날 때까지 delete 를 막기 위해 뮤텍스 락
+	defer mu.Unlock() // 작업 완료 후 해제
+
 	post, err := service.GetPost(id)
 	if err != nil {
 		c.JSON(http.StatusInternalServerError, gin.H{"error": "Failed to get post"})
@@ -56,6 +70,10 @@ func GetPost(c *gin.Context) {
 
 func DeletePost(c *gin.Context) {
 	id := c.Param("id")
+
+	mu.Lock()         // delete 가 끝날 때까지 get 을 막기 위해 뮤텍스 락
+	defer mu.Unlock() // 작업 완료 후 해제
+
 	err := service.DeletePost(id)
 	if err != nil {
 		c.JSON(http.StatusInternalServerError, gin.H{"error": "Failed to delete post"})
